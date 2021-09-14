54ac961479970dc42d5c286e5d49694986017a6a
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2021-09-14 19:46:40 +0200

    ✔ Organize imports

--- client/src/app/components/messages/messages.component.ts
+++ client/src/app/components/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MessagesService } from '../../services/messages/messages.service';
 import { Conversation } from '../../types/Conversation';
 
